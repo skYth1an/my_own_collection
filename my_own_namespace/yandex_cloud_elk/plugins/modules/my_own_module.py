826cbb3185216919536537421e4c4b39f0782e5d
skYth1an <Elgatomolli27072908>
2022-06-03 22:25:00 +0300

    1.0.1

--- my_own_namespace/yandex_cloud_elk/plugins/modules/my_own_module.py
+++ my_own_namespace/yandex_cloud_elk/plugins/modules/my_own_module.py
@@ -8,52 +8,66 @@ __metaclass__ = type
 
 DOCUMENTATION = r'''
 ---
-module: my_own_module
+module: my_test
 
-short_description: module for copy content in path
+short_description: This is my test module
+
+# If this is part of a collection, you need to use semantic versioning,
+# i.e. the version is of the form "2.5.0" and not "2.4".
 version_added: "1.0.0"
+
+description: This is my longer description explaining my test module.
 
 options:
     name:
         description: This is the message to send to the test module.
         required: true
         type: str
-    path:
+    new:
         description:
-        path for create file
-        required: true
-        type: path
-        
-    content:
-        description:
-        content for file
-        required: true
-        type: str
-
+            - Control to demo if the result of this module is changed or not.
+            - Parameter description can be a list as well.
+        required: false
+        type: bool
+# Specify this value according to your collection
+# in format of namespace.collection.doc_fragment_name
+extends_documentation_fragment:
+    - my_namespace.my_collection.my_doc_fragment_name
 
 author:
-    - Igor
+    - Your Name (@yourGitHubHandle)
 '''
 
 EXAMPLES = r'''
+# Pass in a message
 - name: Test with a message
-  my_namespace.my_collection.my_own_module:
-    name: my_name
-- path: path
-  my_namespace.my_collection.my_own_module:
-    path: '/opt/test'
-- content: content
-  my_namespace.my_collection.my_own_module:
-    content: 'test123test'
+  my_namespace.my_collection.my_test:
+    name: hello world
 
+# pass in a message and have changed true
+- name: Test with a message and changed output
+  my_namespace.my_collection.my_test:
+    name: hello world
+    new: true
+
+# fail the module
+- name: Test failure of the module
+  my_namespace.my_collection.my_test:
+    name: fail me
 '''
 
 RETURN = r'''
 # These are examples of possible return values, and in general should use other names for return values.
-
-changed:
-    description: Return state
-
+original_message:
+    description: The original name param that was passed in.
+    type: str
+    returned: always
+    sample: 'hello world'
+message:
+    description: The output message that the test module generates.
+    type: str
+    returned: always
+    sample: 'goodbye'
 '''
 
 from ansible.module_utils.basic import AnsibleModule
@@ -79,7 +93,7 @@ def run_module():
 
     path = module.params['path']
     content = module.params['content']
-    file_path = "~/test/my_file.txt"
+    file_path = path+"/my_file.txt"
 
     if os.path.exists(path):
         result['changed'] = False
